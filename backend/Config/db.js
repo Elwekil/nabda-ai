99fa092a5a9a819cf99e6503c2f5d70744664f7a
Elwekil <marketinggrow242@gmail.com>
2026-09-09 06:16:44 +0300

    Allow TiDB SSL certificate in Vercel

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 4000,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     },
     waitForConnections: true,
     connectionLimit: 10,
